c4f5dbcd22581729290c97691bbf3fa38294cba7
volkandogan <vo.dogan@gmail.com>
2022-12-27 21:16:32 +0300

    first commit

---
+++ components/SimpleText/SimpleTextInput.tsx
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const SimpleTextInput = () => {
+  return (
+    <View>
+      <Text>SimpleText</Text>
+    </View>
+  );
+};
+
+export default SimpleTextInput;
+
+const styles = StyleSheet.create({});
